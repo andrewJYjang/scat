9fcbed85bd03d1b503536a0ebd84884f392d7ea6
andrewJYjang <solution.jang@gmail.com>
2020-08-12 02:31:48 +0900

    PID Controller

---
+++ pid_control.py
@@ -0,0 +1,16 @@
+import time
+
+class PID:
+    """PID Controller
+    """
+
+    def __init__(self, P=0.2, I=0.0, D=0.0):
+        self.Kp = P
+        self.Ki = I
+        self.Kd = D
+
+        self.sample_time = 0.00
+        self.current_time = time.time()
+        self.last_time = self.current_time
+
+        self.clear()
