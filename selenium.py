5e69ecf84f0b22b6d4a1fea92e0601cc08edb9d2
Andrew <solution.jang@gmail.com>
2020-08-18 00:44:52 +0900

    seleminum

--- selenium.py
+++ selenium.py
@@ -4,3 +4,11 @@ driver = webdriver.Chrome('../driver/chromedriver')
 driver.get("http://naver.com")
 
 driver.save_screenshot('../images/001.png')
+
+ele_login = driver.find_element_by_id("id")
+ele_login.clear()
+ele_login.send_keys("appletree")
+
+ele_login = driver.find_element_by_id("pw")
+ele_login.clear()
+ele_login.send_keys("********")
